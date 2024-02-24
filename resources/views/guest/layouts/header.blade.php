e96405991b2fe4993b1b6caab1080ae61e98ac60
Sophia Jade Esclares <sophiajade.esclares@g.msuiit.edu.ph>
2024-02-24 17:42:04 +0800

    welcome page

---
+++ resources/views/guest/layouts/header.blade.php
@@ -0,0 +1,37 @@
+<!-- ======= Header ======= -->
+<header id="header" class="fixed-top d-flex align-items-center header-transparent">
+    <div class="container d-flex justify-content-between align-items-center">
+
+      <div id="logo">
+        <a href=""><img src="{{url('guest/assets/img/logo-header.png')}}"></a>
+        <!-- Uncomment below if you prefer to use a text logo -->
+        <!--<h1><a href="index.html">Regna</a></h1>-->
+      </div>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+          <li><a class="nav-link scrollto" href="#about">About Us</a></li>
+          <li class="dropdown"><a href="#"><span>Arts</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="#">Public Installation</a></li>
+              <li><a href="#">Visual Artworks</a></li>
+              <li><a href="#">Visual Artists</a></li>
+              <li><a href="#">Upcoming Events</a></li>
+              <li><a href="#">Organizations</a></li>
+            </ul>
+          </li>
+          <li class="dropdown"><a href="#"><span>Culture</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="#">People</a></li>
+              <li><a href="#">Places</a></li>
+              <li><a href="#">Cultural Events</a></li>
+              <li><a href="#">Annual Events</a></li>
+            </ul>
+          </li>
+          <li><a class="nav-link scrollto" href="#contact">Contact Us</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+    </div>
+  </header><!-- End Header -->
