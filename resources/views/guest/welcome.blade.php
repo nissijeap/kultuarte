533c4661875a89457d97e37f4f368d9889fae7d6
Sophia Jade Esclares <sophiajade.esclares@g.msuiit.edu.ph>
2024-03-04 22:46:06 +0800

    Updated guest

--- resources/views/guest/welcome.blade.php
+++ resources/views/guest/welcome.blade.php
@@ -165,7 +165,7 @@
     <!-- End Major Events Section -->
 
     <!-- Posts Section -->
-    <div id="posts" class="posts">
+    <div id="posts" class="posts mb-5">
         <div class="container" data-aos="fade-up">
             <div class="row g-5">
                 <div class="col-lg-4">
@@ -278,8 +278,21 @@
     </div>
     <!-- End Posts Section -->
 
-    <!-- ======= Major Events Section ======= -->
-    <section id="event">
+    <!-- ======= Call To Action Section ======= -->
+    <section class="call-to-action mt-3">
+      <div class="container">
+
+        <div class="text-center">
+          <h3>Connect With Us</h3>
+          <p> Your thoughts matter. Drop us a line through our <span style="color: #fcc309; font-weight: bolder;">Contact Us</span> portal.</p>
+          <a class="cta-btn" href="#">Click Me</a>
+        </div>
+
+      </div>
+    </section><!-- End Call To Action Section -->
+
+    <!-- ======= Map Section ======= -->
+    <section id="contact us">
         <div class="container" data-aos="fade-up">
 
             <div class="section-header">
@@ -288,13 +301,79 @@
             </div>
 
             <div class="row">
-                
+                <iframe style="border:0; width: 100%; height: 500px;" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" allowfullscreen></iframe>
             </div>
 
         </div>
     </section>
-    <!-- End Major Events Section -->
+    <!-- End Map Section -->
 
+    <!-- ======= Frequently Asked Questions Section ======= -->
+    <div id="faq" class="faq mb-5 pb-3">
+      <div class="container">
+        <div class="section-header">
+            <h2>FAQs</h2>
+            <p>Elevate your <span>knowledge base</span> with our FAQs</p>
+        </div>
+            <ul class="faq-list">
+
+            <li data-aos="fade-up">
+                <a data-bs-toggle="collapse" class="collapsed" data-bs-target="#faq1">Non consectetur a erat nam at lectus urna duis? <i class="bx bx-chevron-down icon-show"></i><i class="bx bx-x icon-close"></i></a>
+                <div id="faq1" class="collapse" data-bs-parent=".faq-list">
+                <p>
+                    Feugiat pretium nibh ipsum consequat. Tempus iaculis urna id volutpat lacus laoreet non curabitur gravida. Venenatis lectus magna fringilla urna porttitor rhoncus dolor purus non.
+                </p>
+                </div>
+            </li>
+
+            <li data-aos="fade-up" data-aos-delay="100">
+                <a data-bs-toggle="collapse" data-bs-target="#faq2" class="collapsed">Feugiat scelerisque varius morbi enim nunc faucibus a pellentesque? <i class="bx bx-chevron-down icon-show"></i><i class="bx bx-x icon-close"></i></a>
+                <div id="faq2" class="collapse" data-bs-parent=".faq-list">
+                <p>
+                    Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Id interdum velit laoreet id donec ultrices. Fringilla phasellus faucibus scelerisque eleifend donec pretium. Est pellentesque elit ullamcorper dignissim. Mauris ultrices eros in cursus turpis massa tincidunt dui.
+                </p>
+                </div>
+            </li>
+
+            <li data-aos="fade-up" data-aos-delay="200">
+                <a data-bs-toggle="collapse" data-bs-target="#faq3" class="collapsed">Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi? <i class="bx bx-chevron-down icon-show"></i><i class="bx bx-x icon-close"></i></a>
+                <div id="faq3" class="collapse" data-bs-parent=".faq-list">
+                <p>
+                    Eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Faucibus pulvinar elementum integer enim. Sem nulla pharetra diam sit amet nisl suscipit. Rutrum tellus pellentesque eu tincidunt. Lectus urna duis convallis convallis tellus. Urna molestie at elementum eu facilisis sed odio morbi quis
+                </p>
+                </div>
+            </li>
+
+            <li data-aos="fade-up" data-aos-delay="300">
+                <a data-bs-toggle="collapse" data-bs-target="#faq4" class="collapsed">Ac odio tempor orci dapibus. Aliquam eleifend mi in nulla? <i class="bx bx-chevron-down icon-show"></i><i class="bx bx-x icon-close"></i></a>
+                <div id="faq4" class="collapse" data-bs-parent=".faq-list">
+                <p>
+                    Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Id interdum velit laoreet id donec ultrices. Fringilla phasellus faucibus scelerisque eleifend donec pretium. Est pellentesque elit ullamcorper dignissim. Mauris ultrices eros in cursus turpis massa tincidunt dui.
+                </p>
+                </div>
+            </li>
+
+            <li data-aos="fade-up" data-aos-delay="400">
+                <a data-bs-toggle="collapse" data-bs-target="#faq5" class="collapsed">Tempus quam pellentesque nec nam aliquam sem et tortor consequat? <i class="bx bx-chevron-down icon-show"></i><i class="bx bx-x icon-close"></i></a>
+                <div id="faq5" class="collapse" data-bs-parent=".faq-list">
+                <p>
+                    Molestie a iaculis at erat pellentesque adipiscing commodo. Dignissim suspendisse in est ante in. Nunc vel risus commodo viverra maecenas accumsan. Sit amet nisl suscipit adipiscing bibendum est. Purus gravida quis blandit turpis cursus in
+                </p>
+                </div>
+            </li>
+
+            <li data-aos="fade-up" data-aos-delay="500">
+                <a data-bs-toggle="collapse" data-bs-target="#faq6" class="collapsed">Tortor vitae purus faucibus ornare. Varius vel pharetra vel turpis nunc eget lorem dolor? <i class="bx bx-chevron-down icon-show"></i><i class="bx bx-x icon-close"></i></a>
+                <div id="faq6" class="collapse" data-bs-parent=".faq-list">
+                <p>
+                    Laoreet sit amet cursus sit amet dictum sit amet justo. Mauris vitae ultricies leo integer malesuada nunc vel. Tincidunt eget nullam non nisi est sit amet. Turpis nunc eget lorem dolor sed. Ut venenatis tellus in metus vulputate eu scelerisque. Pellentesque diam volutpat commodo sed egestas egestas fringilla phasellus faucibus. Nibh tellus molestie nunc non blandit massa enim nec.
+                </p>
+                </div>
+            </li>
+            </ul>
+      </div>
+    </div>
+    <!-- End Frequently Asked Questions Section -->
 
 </main>
 
